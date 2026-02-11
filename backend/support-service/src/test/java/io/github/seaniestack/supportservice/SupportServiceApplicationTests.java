161fad74b943c162e5b1509de3b2a6c9675b3e01
SeanieStack <seaniestack345@gmail.com>
2026-02-11 16:11:45 +0000

    Refactored backend into to services

---
+++ backend/support-service/src/test/java/io/github/seaniestack/supportservice/SupportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.seaniestack.supportservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
